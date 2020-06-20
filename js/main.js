2d33c930d73a96458907f04ee38ed2ebc437fdf5
Juan_Cundafé <juan.cundafe@crowdsense.app>
2020-06-20 12:42:24 -0500

    se imprime en general

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@
   - - -MANTENER EL CÓDIGO COMENTADO Y ORGANIZADO
   - - -CHECAR EN QUE RAMA SE ESTA MODIFICANDO
  */
+
 /* Var global scope */
 var postObject = {};
 var postObjectArr = [];
 
+
 /* Guardar la información del post creado*/
 const getNewPost = () => {
-  let date = new Date();
+      // instantiate a moment object
+  var NowMoment = moment();
+  let date = NowMoment.format('MMM-D');
   console.log(date);
   $(".form-control").each(function (index) {
     let value = $(this).val();
@@ -58,6 +62,8 @@ const printPostCard = (postObject, key) => {
   </div>
 </div>`);
 };
+
+
 const getPostDb = () => {
   $.get(
     "https://ajaxclass-1ca34.firebaseio.com/mediumBlog/Equipo4/post/.json",
@@ -67,10 +73,266 @@ const getPostDb = () => {
         postObjectArr[key] = value;
       });
       //addShowModalListeners();
+      printGeneral1(postObjectArr);
+      printGeneral2(postObjectArr);
+      
     }
   );
 };
-console.log(postObjectArr);
+
+
+
+
+//imprime post en la sección general 1. Se manda a llamar en la función getPostDb para obtener el array que regresa de la base de datos
+const printGeneral1 = (array) => { 
+for(key in array){
+  $(".general-1").append(`<article class="mb-4">
+  <div class="row d-flex">
+    <div class="col-8">
+      <div class="pl-1 md-1">
+        <p class="text-muted my-0">
+          <small class="card-text">BUSINESS Popular topic</small>
+        </p>
+        <h5 class="card-text my-0">
+        ${array[key].title}
+        </h5>
+        <p class="text-muted my-0 d-none d-md-inline">
+          <small class="card-text">${array[key].contenido}</small>
+        </p>
+        <div class="d-flex justify-content-between pr-2">
+          <div>
+            <small class="text-muted">
+              <a href="#" class="text-dark">${array[key].autor}</a>
+            </small>
+
+            <small class="text-muted"
+              ><p class="card-text">
+              ${array[key].date} &#183; 3 min read
+                <i class="p-2 fas fa-star"></i></p
+            ></small>
+          </div>
+          <div class="font-weight-lighter text-muted">
+            <i class="far fa-bookmark"></i>
+            <a href="#" class="card-link text-dark text-muted"
+              ><i class="fas fa-ellipsis-h ml-3"></i
+            ></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-4">
+      <img
+         src=${array[key].image}
+        class="img-fluid"
+        alt="Responsive image"
+      />
+    </div>
+  </div>
+</article>`);
+}
+};
+
+//imprime post en la sección general 2. Se manda a llamar en la función getPostDb para obtener el array que regresa de la base de datos
+const printGeneral2 = (array) => { 
+    for(key in array){
+      $(".general-2").append(`<article class="mb-4">
+      <div class="row d-flex">
+        <div class="col-8">
+          <div class="pl-1 md-1">
+            <p class="text-muted my-0">
+              <small class="card-text">BUSINESS Popular topic</small>
+            </p>
+            <h5 class="card-text my-0">
+            ${array[key].title}
+            </h5>
+            <p class="text-muted my-0 d-none d-md-inline">
+              <small class="card-text">${array[key].contenido}</small>
+            </p>
+            <div class="d-flex justify-content-between pr-2">
+              <div>
+                <small class="text-muted">
+                  <a href="#" class="text-dark">${array[key].autor}</a>
+                </small>
+    
+                <small class="text-muted"
+                  ><p class="card-text">
+                  ${array[key].date} &#183; 3 min read
+                    <i class="p-2 fas fa-star"></i></p
+                ></small>
+              </div>
+              <div class="font-weight-lighter text-muted">
+                <i class="far fa-bookmark"></i>
+                <a href="#" class="card-link text-dark text-muted"
+                  ><i class="fas fa-ellipsis-h ml-3"></i
+                ></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <img
+             src=${array[key].image}
+            class="img-fluid"
+            alt="Responsive image"
+          />
+        </div>
+      </div>
+    </article>`);
+    }
+    };
+
+// imprimir primera sección de recientes con el post en [0]
+/*
+ 
+     const recentPost = (array1) => {
+      let { title, autor, contenido, image, date } = array1[0];
+         //destructuración del objeto específico del arreglo
+            $(".style1").append(`<article class="mb-4">
+            <div class="card text-left border-white">
+              <img
+                height="150"
+                src=${array1[key].image}
+                class="card-img-top img-sec1"
+              />
+              <div class="card-body text-left p-card1">
+                <h5 class="text-left">
+                  ${array1[key].title}
+                </h5>
+                <p class="text-muted parafos-card">
+                  ${array1[key].contenido}
+                </p>
+                <div class="d-flex justify-content-between">
+                  <div>
+                    <small class="text-muted">
+                      <a href="#" class="text-dark"> ${array1[key].autor} </a>
+                      <span class="text-dark">in</span>
+                      <a href="#" class="text-dark">Mind cafe </a>
+                    </small>
+
+                    <small class="text-muted"
+                      ><p class="card-text">
+                        ${array1[key].date} &middot; 6 min read
+                        <i class="p-2 fas fa-star"></i></p
+                    ></small>
+                  </div>
+                  <div class="font-weight-lighter text-muted">
+                    <svg
+                      class="bi bi-three-dots"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
+                      />
+                    </svg>
+                    <a href="#" class="card-link text-dark text-muted"
+                      ><i class="fas fa-ellipsis-h ml-3"></i
+                    ></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>`);
+     }
+        */
+
+
+// imprimir primera sección de recientes con el post en [1,2,3]
+/*
+var arraySection2 = []
+for(key in array){
+ $(".style1").append(`<article class="mb-4">
+              <div class="row d-flex flex-row-reverse flex-md-row no-gutters">
+                <div class="col-4">
+                  <img
+                    src=${array1[key].image}
+                    class="ml-4 ml-md-0"
+                  />
+                </div>
+                <div class="col-8 col-">
+                  <div class="pl-md-2">
+                    <h6 class="">
+                      ${array1[key].title}
+                    </h6>
+                    <div class="d-flex justify-content-between">
+                      <div>
+                        <small class="text-muted">
+                          <a href="#" class="text-dark">${array1[key].autor} </a>
+                          <span class="text-dark">in</span>
+                          <a href="#" class="text-dark">Heated </a>
+                        </small>
+
+                        <small class="text-muted"
+                          ><p class="card-text">
+                           ${array1[key].date}  &#183; 6 min read
+                            <i class="p-2 fas fa-star"></i></p
+                        ></small>
+                      </div>
+                      <div class="font-weight-lighter text-muted">
+                        <i
+                          class="far fa-bookmark d-md-none d--inline-block"
+                        ></i>
+                        <a href="#" class="card-link text-dark text-muted"
+                          ><i class="fas fa-ellipsis-h ml-3"></i
+                        ></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>`);
+}
+*/
+
+// imprimir primera sección de recientes con el post en [4]
+/*
+let { title, autor, contenido, image, date } = array1[4]
+$(".style1").append(`<article class="mb-4 d-none d-lg-block">
+            <div class="card text-left border-white">
+              <img
+                height="150"
+                src=${array1[key].image}
+                class="card-img-top"
+              />
+              <div class="mt-2 mr-5 text-left p-card3">
+                <h5 class="text-left">
+                  ${array1[key].title}
+                </h5>
+                <p class="text-muted parafos-card">
+                  ${array1[key].contenido}
+                </p>
+
+                <p></p>
+                <div class="d-flex justify-content-between">
+                  <div>
+                    <small class="text-muted">
+                      <a href="#" class="text-dark"> ${array1[key].autor} </a>
+                      <span class="text-dark">in</span>
+                      <a href="#" class="text-dark">Payne </a>
+                    </small>
+
+                    <small class="text-muted"
+                      ><p class="card-text">
+                       ${array1[key].date} &middot; 5 min read
+                        <i class="p-2 fas fa-star"></i></p
+                    ></small>
+                  </div>
+                  <div class="font-weight-lighter text-muted">
+                    <i class="far fa-bookmark d-md-none d--inline-block"></i>
+                    <a href="#" class="card-link text-dark"
+                      ><i class="fas fa-ellipsis-h ml-3"></i
+                    ></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>`);
+
+*/
 /*
 const printModal = (response) => {
   let { title, autor, contenido, image, date } = response;
