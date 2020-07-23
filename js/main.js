84b1c03ac86e7e543a46e3c4e379fbbc746b68d0
Arianaomi <arianaomi.lp@gmail.com>
2020-07-23 18:32:12 -0500

    :zap: Arreglando imagenes
    Arreglando imagenes

--- js/main.js
+++ js/main.js
@@ -4,71 +4,68 @@
  */
 
 /* Var global scope */
-var postObject = {};
-var postObjectArr = [];
-var postWithKeyObject = {};
-var postWithKeyObjectArr = [];
+var postObject = {}
+var postObjectArr = []
+var postWithKeyObject = {}
+var postWithKeyObjectArr = []
 
 /* Dynamic HTML */
 const loadContent = (contentUrl, event) => {
-  event.preventDefault();
-  $("#content-wrapper").load(contentUrl);
-};
+  event.preventDefault()
+  $('#content-wrapper').load(contentUrl)
+}
 /* Guardar la información del post creado*/
 const saveNewPost = () => {
-  let date = new Date();
+  let date = new Date()
   const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dic",
-  ];
-  let date2 = months[date.getMonth()] + " " + date.getDate();
-  let milisegundos = date.getTime();
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dic',
+  ]
+  let date2 = months[date.getMonth()] + ' ' + date.getDate()
+  let milisegundos = date.getTime()
   //console.log(milisegundos);
-  let popularFlag = $(".form-check-input").is(":checked") ? true : false;
+  let isPopular = $('.form-check-input').is(':checked') ? true : false
   //console.log(popularFlag);
 
-  $(".form-control").each(function (index) {
-    let value = $(this).val();
-    let propertyKey = $(this).data("property-key");
-    postObject[propertyKey] = value;
-    postObject["date"] = date2;
-    postObject["popularFlag"] = popularFlag;
-    postObject["compararFechas"] = milisegundos;
-  });
-  //console.log(postObject);
-  uploadPost(postObject);
-};
+  $('.form-control').each(function (index) {
+    let value = $(this).val()
+    let propertyKey = $(this).data('property-key')
+    postObject[propertyKey] = value
+    //postObject['date'] = date2
+    postObject['isPopular'] = isPopular
+    //postObject['compararFechas'] = milisegundos
+  })
+  console.log(postObject)
+  uploadPost(postObject)
+}
 
 /* Sube al endpoint el post creado y nos regresa la llave con lo que lo guardo */
-const uploadPost = (postObject) => {
-  $.post(
-    "https://ajaxclass-1ca34.firebaseio.com/mediumBlog/Equipo4/post/.json",
-    JSON.stringify(postObject),
-    function (data) {
-      $("#successModal").modal("show");
-      ////console.log(data);
-    }
-  );
-};
+const uploadPost = postObject => {
+  console.log(postObject)
+  $.post('http://localhost:8080/entries', postObject, function (data) {
+    $('#successModal').modal('show')
+
+  }),
+    'json'
+}
 
 /* Crear la card con los datos de db */
 const printGeneralCard = (container, postObject, key) => {
-  let { title, subtitle, autor, image, date, postKey } = postObject;
-  let postWrapper = $(container);
-  postWrapper.innerHTML = "";
+  let { title, subtitle, autor, image, date, postKey } = postObject
+  let postWrapper = $(container)
+  postWrapper.innerHTML = ''
   $(container).append(`
-  <article class="my-4 generalCards">
-    <div class="row d-flex no-gutters">
+  <article class="row d-flex no-gutters my-4 generalCards">
       <div class="col-8">
         <div class="pl-1 md-1">
           <p class="text-muted my-0">
@@ -110,21 +107,21 @@ const printGeneralCard = (container, postObject, key) => {
           </div>
         </div>
       </div>
-      <div class="col-4">
+      <div class="col-4 ">
         <img
           src="${image}"
-          class="img-fluid w-100  imgeneral"
+          class="img-fluid imgdos "
           alt="Responsive image"
         />
       </div>
-    </div>
-  </article>`);
-};
+   
+  </article>`)
+}
 
 const printRecents1 = (container, postObject, key) => {
-  let { title, subtitle, autor, image, date, postKey } = postObject;
-  let postWrapper = $(container);
-  postWrapper.innerHTML = "";
+  let { title, subtitle, autor, image, date, postKey } = postObject
+  let postWrapper = $(container)
+  postWrapper.innerHTML = ''
   $(container).append(`<article class="mb-4">
   <div class="card text-left border-white">
     <img
@@ -170,16 +167,16 @@ const printRecents1 = (container, postObject, key) => {
       </div>
     </div>
   </div>
-</article>`);
-};
+</article>`)
+}
 
 const printRecents2 = (container, postObject, key) => {
-  let { title, subtitle, autor, image, date, postKey } = postObject;
-  let postWrapper = $(container);
-  postWrapper.innerHTML = "";
+  let { title, subtitle, autor, image, date, postKey } = postObject
+  let postWrapper = $(container)
+  postWrapper.innerHTML = ''
   $(container).append(`<article class=" general3cards mb-4">
   <div class="row d-flex flex-row-reverse flex-md-row no-gutters">
-    <div class="col-4">
+    <div class="col-4 imgrecientess" >
       <img src=${image} class=" ml-1 ml-md-0 ml-md-0 img3cards  "/>
     </div>
     <div class="col-8 col-">
@@ -218,13 +215,13 @@ const printRecents2 = (container, postObject, key) => {
       </div>
     </div>
   </div>
-</article>`);
-};
+</article>`)
+}
 
 const printRecents3 = (container, postObject, key) => {
-  let { title, subtitle, autor, image, date, postKey } = postObject;
-  let postWrapper = $(container);
-  postWrapper.innerHTML = "";
+  let { title, subtitle, autor, image, date, postKey } = postObject
+  let postWrapper = $(container)
+  postWrapper.innerHTML = ''
   $(container).append(`<article class="mb-4 d-none d-lg-block">
   <div class="card text-left border-white">
     <img
@@ -269,15 +266,15 @@ const printRecents3 = (container, postObject, key) => {
       </div>
     </div>
   </div>
-</article>`);
-};
+</article>`)
+}
 
 //Imprime la lista de los populares
 const printPostPoPular = (count, postObject, key) => {
-  let { title, autor, date } = postObject;
-  let postWrapper = $(".popular");
-  postWrapper.innerHTML = "";
-  $(".popular").append(`
+  let { title, autor, date } = postObject
+  let postWrapper = $('.popular')
+  postWrapper.innerHTML = ''
+  $('.popular').append(`
   <li class="d-flex mb-2">
                 <div class="num-popular mt-1">
               0${count}
@@ -295,165 +292,182 @@ const printPostPoPular = (count, postObject, key) => {
                     </small>
                   </div>
                 </div>
-              </li>`);
-};
+              </li>`)
+}
 //Creando array con los más recientes
 const getmostRecent = () => {
-  let mostRecent;
+  let mostRecent
   mostRecent = postWithKeyObjectArr
     .sort(function (currentItem, nextItem) {
-      return nextItem.compararFechas + currentItem.comprararFechas;
+      return nextItem.compararFechas + currentItem.comprararFechas
     })
     .reverse()
-    .slice(0, 5);
+    .slice(0, 5)
   //console.log(mostRecent);
-  return mostRecent;
-};
+  return mostRecent
+}
 
 //Creando array con los post populares
 const getmostPopular = () => {
-  let mostPopular;
-  let count = 1;
-  mostPopular = postWithKeyObjectArr.filter((post) => {
-    return post.popularFlag == true;
-  });
+  let mostPopular
+  let count = 1
+
+  mostPopular = postWithKeyObjectArr.filter(post => post.isPopular == true)
   //console.log(postWithKeyObjectArr);
-  mostPopular = mostPopular.reverse().slice(0, 4);
-  //console.log(mostPopular);
-  return mostPopular;
-};
+  mostPopular = mostPopular.reverse().slice(0, 4)
+  return mostPopular
+}
 
 const general1 = () => {
-  let general1;
+  let general1
   //postWithKeyObjectArr4 = postWithKeyObjectArr
-  general1 = postWithKeyObjectArr.slice(2, 6);
+  general1 = postWithKeyObjectArr.slice(2, 6)
   //console.log(general1);
-  return general1;
-};
+  return general1
+}
 
 //Funcion que imprimira todo
 const printDOM = () => {
-  let key;
-  let count = 1;
-  let generalPosts1 = [];
+  let key
+  let count = 1
+  let generalPosts1 = []
   //let section1Arr = postWithKeyObjectArr.splice(0, 4);
   ////console.log(postWithKeyObjectArr);
-  let popular = [];
-  popular = getmostPopular();
-  let recent = [];
-  recent = getmostRecent();
-  generalPosts1 = general1();
+  let popular = []
+  popular = getmostPopular()
+  let recent = []
+  recent = getmostRecent()
+  generalPosts1 = general1()
   //console.log(recent);
   //console.log(popular);
-  let section1Recents = recent.slice(0, 1);
-  let section2Recents = recent.slice(1, 4);
-  let section3Recents = recent.slice(4, 5);
+  let section1Recents = recent.slice(0, 1)
+  let section2Recents = recent.slice(1, 4)
+  let section3Recents = recent.slice(4, 5)
 
-  popular.forEach((post) => {
-    key = post.postKey;
-    printPostPoPular(count, post, key);
-    count++;
-  });
+  popular.forEach(post => {
+    key = post.postKey
+    printPostPoPular(count, post, key)
+    count++
+  })
 
-  generalPosts1.forEach((post) => {
-    key = post.postKey;
-    printGeneralCard(".general-1", post, key);
-  });
-  postWithKeyObjectArr.forEach((post) => {
-    key = post.postKey;
-    printGeneralCard(".general-2", post, key);
-  });
-  section1Recents.forEach((post) => {
-    key = post.postKey;
-    printRecents1(".style1", post, key);
-  });
+  generalPosts1.forEach(post => {
+    key = post.postKey
+    printGeneralCard('.general-1', post, key)
+  })
+  postWithKeyObjectArr.forEach(post => {
+    key = post.postKey
+    printGeneralCard('.general-2', post, key)
+  })
+  section1Recents.forEach(post => {
+    key = post.postKey
+    printRecents1('.style1', post, key)
+  })
   ////console.log(section2Recents);
 
-  section2Recents.forEach((post) => {
-    key = post.postKey;
-    printRecents2(".style2", post, key);
-  });
+  section2Recents.forEach(post => {
+    key = post.postKey
+    printRecents2('.style2', post, key)
+  })
   //console.log(section2Recents);
 
-  section3Recents.forEach((post) => {
-    key = post.postKey;
-    printRecents3(".style3", post, key);
-  });
+  section3Recents.forEach(post => {
+    key = post.postKey
+    printRecents3('.style3', post, key)
+  })
   //console.log(section3Recents);
-};
-/* Request para obtener los post, imprimirlos y crear arreglo */
+}
 const getPostDb = () => {
+  $.get('http://localhost:8080/entries', function (response) {
+    response.data.forEach(entries => {
+      const { _id, writer } = entries
+      console.log('hola writer:', writer)
+      console.log(entries)
+      postObjectArr[_id] = entries
+      postWithKeyObject = { ...entries, postKey: _id, autor: writer.name }
+      postWithKeyObjectArr.push(postWithKeyObject)
+    })
+    printDOM()
+    console.log(postWithKeyObjectArr)
+    addShowModalListeners()
+  })
+}
+/* Request para obtener los post, imprimirlos y crear arreglo */
+/*const getPostDb = () => {
   $.get(
-    "https://ajaxclass-1ca34.firebaseio.com/mediumBlog/Equipo4/post/.json",
+    'https://ajaxclass-1ca34.firebaseio.com/mediumBlog/Equipo4/post/.json',
     function (response) {
       $.each(response, (key, value) => {
+        console.log('response', response)
+        console.log('key', key)
+        console.log('value', value)
         //printPostSection1Card(value, key);
         // postObject = response;
-        postObjectArr[key] = value;
-        postWithKeyObject = { ...value, postKey: key };
-        postWithKeyObjectArr.push(postWithKeyObject);
-      });
-      printDOM();
-      console.log(postWithKeyObjectArr);
-      addShowModalListeners();
+        postObjectArr[key] = value
+        console.log(postObjectArr)
+        postWithKeyObject = { ...value, postKey: key }
+        postWithKeyObjectArr.push(postWithKeyObject)
+      })
+      printDOM()
+      console.log(postWithKeyObjectArr)
+      addShowModalListeners()
     }
-  );
-};
+  )
+}*/
 /* Imprimir modal */
-const printModal = (selectedPost) => {
-  let { title, subtitle, autor, content, image } = selectedPost;
+const printModal = selectedPost => {
+  let { title, subtitle, autor, content, image } = selectedPost
 
-  $("#articleModal").find(".modal-content");
-  $("#articleModal").find(".modal-title").text(title);
-  $("#articleModal").find(".modal-subtitle").text(subtitle);
-  $("#articleModal").find(".modal-autor").text(autor);
-  $("#articleModal").find(".modal-img").attr("src", image);
-  $("#articleModal").find(".contenido").text(content);
-  $("#articleModal").modal("show");
-};
+  $('#articleModal').find('.modal-content')
+  $('#articleModal').find('.modal-title').text(title)
+  $('#articleModal').find('.modal-subtitle').text(subtitle)
+  $('#articleModal').find('.modal-autor').text(autor)
+  $('#articleModal').find('.modal-img').attr('src', image)
+  $('#articleModal').find('.contenido').text(content)
+  $('#articleModal').modal('show')
+}
 
 /* conseguir key de post para llamar la modal */
 const conseguirModal = () => {
-  event.preventDefault();
-  let keyPost = $(event.target).attr("id");
-  console.log("key: " + keyPost);
-  let selectedPost = postObjectArr[keyPost];
+  event.preventDefault()
+  let keyPost = $(event.target).attr('id')
+  console.log('key: ' + keyPost)
+  let selectedPost = postObjectArr[keyPost]
   //console.log(postObjectArr);
-  printModal(selectedPost);
-};
+  printModal(selectedPost)
+}
 
 /* Eventos  */
 
 const addShowModalListeners = () => {
-  let aListener = document.querySelectorAll(".showModal");
-  aListener.forEach((a) => {
-    a.addEventListener("click", conseguirModal);
-  });
-};
+  let aListener = document.querySelectorAll('.showModal')
+  aListener.forEach(a => {
+    a.addEventListener('click', conseguirModal)
+  })
+}
 
 /* SCROLL infinito */
-var viewportHeight = window.innerHeight;
-window.addEventListener("scroll", (event) => {
-  let scollPosition = window.scrollY;
+var viewportHeight = window.innerHeight
+window.addEventListener('scroll', event => {
+  let scollPosition = window.scrollY
   // console.log(window.scrollY);
   scollPosition > viewportHeight
-    ? postWithKeyObjectArr.forEach((post) => {
-        key = post.postKey;
-        printGeneralCard(".general-2", post, key);
+    ? postWithKeyObjectArr.forEach(post => {
+        key = post.postKey
+        printGeneralCard('.general-2', post, key)
       })
-    : null;
-});
-getPostDb();
-let viewportWidth = $(window).innerWidth();
+    : null
+})
+getPostDb()
+let viewportWidth = $(window).innerWidth()
 
-const navbarScroll = (direction) => {
-  if (direction === "left" && viewportWidth < 1000) {
-    $("#nav-inside").animate({ scrollLeft: "-=50px" }, "fast");
-  } else if (direction === "right" && viewportWidth < 1000) {
-    $("#nav-inside").animate({ scrollLeft: "+=50px" }, "fast");
-  } else if (direction === "left" && viewportWidth > 1000) {
-    $("#nav-inside").animate({ scrollLeft: "0px" }, "fast");
-  } else if (direction === "right" && viewportWidth > 1000) {
-    $("#nav-inside").animate({ scrollLeft: "950px" }, "fast");
+const navbarScroll = direction => {
+  if (direction === 'left' && viewportWidth < 1000) {
+    $('#nav-inside').animate({ scrollLeft: '-=50px' }, 'fast')
+  } else if (direction === 'right' && viewportWidth < 1000) {
+    $('#nav-inside').animate({ scrollLeft: '+=50px' }, 'fast')
+  } else if (direction === 'left' && viewportWidth > 1000) {
+    $('#nav-inside').animate({ scrollLeft: '0px' }, 'fast')
+  } else if (direction === 'right' && viewportWidth > 1000) {
+    $('#nav-inside').animate({ scrollLeft: '950px' }, 'fast')
   }
-};
+}
